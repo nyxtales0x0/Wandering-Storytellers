be39943611843fd8b9171fc9f5e04bef94a79c86
nyxtales0x0 <birblikescoffee@gmail.com>
2022-12-18 18:40:52 +0530

    [Edit] subscene : start of scene : tavernIntro

--- chapters/chapter1/scenes/tavernIntro.js
+++ chapters/chapter1/scenes/tavernIntro.js
@@ -1,9 +1,9 @@
 export const start = {
-    text: `In a calm evening, as the winds grazing past flowing along your skin, you find yourself standing in the town square of a city called Vernes, a city you recently moved into after you were released from your job as a soldier of the King's army. The town square is busy as usual making it look like the liveliest part of the city.
+    text: `In a calm evening, as the winds grazes past you and flows along your skin, you find yourself standing in the town square of a city called Vernes, a city you recently moved into after you were released from your job as a soldier of the King's army. The town square is busy as usual making it look like the liveliest part of the city.
     
     You look at the old tavern settled on a stone foundation and can see the sun setting behind it. You take a deep sigh, all that your mind cares about right now is a nice warm piece of bread and honey along with a mug of refreshing ale. "I deserve it after all the days work" you think to yourself.
     
-    As your stomach starts to grawl in excitement, you move forward and open the door of the Winter Bear Tavern, the warmth flushing at you as the door opens.
+    As your stomach starts to growl in excitement, you move forward and open the door of the Winter Bear Tavern, the warmth flushing at you as the door opens.
     
     "Welcome to Winter Bear tavern young fellow."`,
     choices: [
