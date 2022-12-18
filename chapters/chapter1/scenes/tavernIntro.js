6e8a9b95955aadb93569351a74c70e540a3ecbd6
LeekyBoi123 <120955346+LeekyBoi123@users.noreply.github.com>
2022-12-18 23:08:23 +0700

    Update tavernIntro.js
    Under no circumstance would I disclose in clear terms of what I had changed. You're welcome to try and pry it from me.

--- chapters/chapter1/scenes/tavernIntro.js
+++ chapters/chapter1/scenes/tavernIntro.js
@@ -1,11 +1,11 @@
 export const start = {
     text: `In a calm evening, as the winds grazes past you and flows along your skin, you find yourself standing in the town square of a city called Vernes, a city you recently moved into after you were released from your job as a soldier of the King's army. The town square is busy as usual making it look like the liveliest part of the city.
     
-    You look at the old tavern settled on a stone foundation and can see the sun setting behind it. You take a deep sigh, all that your mind cares about right now is a nice warm piece of bread and honey along with a mug of refreshing ale. "I deserve it after all the days work" you think to yourself.
+    You look at the old tavern settled on a stone foundation and can see the sun setting behind it. You take a deep sigh, all that your mind cares about right now is a nice warm loaf of bread and honey along with a mug of refreshing ale. "I deserve it after all the days work", you think to yourself.
     
     As your stomach starts to growl in excitement, you move forward and open the door of the Winter Bear Tavern, the warmth flushing at you as the door opens.
     
-    "Welcome to Winter Bear tavern young fellow."`,
+    "Welcome to Winter Bear's tavern young fellow."`,
     choices: [
         {
             text: "Continue",
@@ -17,6 +17,6 @@ export const start = {
 }
 
 const helloRalf = {
-    text: `"Welcome to winter bear's tavern.`,
+    text: `"Welcome to Winter Bear's tavern.`,
     choices: []
 }
