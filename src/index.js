d792b4da2b45ad169bedcb8f3ceaaad5c46c5174
nyxtales0x0 <birblikescoffee@gmail.com>
2022-12-22 20:27:07 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { gameManager } from "./engine/engine.js";
 import { tavernIntro } from "./chapters/chapter1/index.js";
 
-gameManager.setfirstScene(tavernIntro.start)
+gameManager.setfirstScene(tavernIntro.start);
 
 gameManager.startGame();
